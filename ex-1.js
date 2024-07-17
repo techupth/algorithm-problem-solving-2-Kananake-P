44e6bb177773235b8d742ec1c31b68ef833ddfc4
Kan <kanalovefootball@gmail.com>
2024-07-17 15:27:53 +0700

    create: ex:-1 , ex-2

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,19 @@
+let result = -1;
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let n = 0; n < students.length; n++) {
+    if (searchStudent === students[n]) {
+      return n;
+    }
+  }
+  return result;
 }
 
+const students = ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"];
+console.log(findStudentIndex(students, "Andrew"));
+
+// const students =  ["Andrew", "Bobby", "Diana", "George", "Hannah", "Isaac", "Jasmine", "John"]
+// console.log(findStudentIndex(students, "John"));
+
 // ตอบคำถามตรงนี้จ้า
+// ANS: 2**n
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,24 @@
+let result = -1;
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let n = 0; n < books.length; n++) {
+    if (searchTitle === books[n].title) {
+      return n;
+    }
+  }
+  return result;
 }
+
+const books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+
+console.log(findBookIndex(books, "Gone with the Wind"));
+console.log(findBookIndex(books, "The Master Mind"));
