6580e3bb708b4e4e75ddb396b7b605dd0d70085a
Kan <kanalovefootball@gmail.com>
2024-07-17 21:34:55 +0700

    edit: ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,19 +1,53 @@
-let result = -1;
 function findStudentIndex(students, searchStudent) {
-  // เริ่มเขียนโค้ดตรงนี้จ้า
-  for (let n = 0; n < students.length; n++) {
-    if (searchStudent === students[n]) {
-      return n;
+  let left = 0;
+  let right = students.length - 1;
+  let index = -1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (students[mid] === searchStudent) {
+      index = mid;
+      break;
+    } else if (students[mid] < searchStudent) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
-  return result;
+  return index;
 }
 
 const students = ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"];
 console.log(findStudentIndex(students, "Andrew"));
 
-// const students =  ["Andrew", "Bobby", "Diana", "George", "Hannah", "Isaac", "Jasmine", "John"]
+// ตอบคำถามตรงนี้จ้า
+// ANS: log n
+
+// const students = [
+//   "Andrew",
+//   "Bobby",
+//   "Diana",
+//   "George",
+//   "Hannah",
+//   "Isaac",
+//   "Jasmine",
+//   "John",
+// ];
 // console.log(findStudentIndex(students, "John"));
 
-// ตอบคำถามตรงนี้จ้า
-// ANS: 2**n
+// let result = -1;
+// function findStudentIndex(students, searchStudent) {
+//   // เริ่มเขียนโค้ดตรงนี้จ้า
+//   for (let n = 0; n < students.length; n++) {
+//     if (searchStudent === students[n]) {
+//       return n;
+//     }
+//   }
+//   return result;
+// }
+
+// const students = ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"];
+// console.log(findStudentIndex(students, "Andrew"));
+
+// const students =  ["Andrew", "Bobby", "Diana", "George", "Hannah", "Isaac", "Jasmine", "John"]
+// console.log(findStudentIndex(students, "John"));
